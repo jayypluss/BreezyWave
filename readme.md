ce238531b6e7d754ef8c71a73cba0cddda0f00ea
jayypluss <jonathanfk1@gmail.com>
2022-05-02 14:00:32 +0000

    Deploying to gh-pages from  @ 8fa0d784c884e281a06714ed9788694d5d42489b 🚀

--- readme.md
+++
@@ -1,3 +0,0 @@
-# BreezyWave
-
-Currently protype game made on [Godot Engine](https://github.com/godotengine/godot) using [godot-game-template](https://github.com/crystal-bit/godot-game-template#change-scene-and-show-progress-bar).
--- scenes/gameplay/TrippyGround.gd
+++
@@ -1,8 +0,0 @@
-extends Spatial
-
-
-func _physics_process(delta):
-	if Input.is_action_just_pressed("jump"):
-		var random = randi() % 40 + 1
-		print("Changing shader random to: ", random)
-		$StaticBody.get_child(1).get_surface_material(0).set_shader_param("random_scale", random)
