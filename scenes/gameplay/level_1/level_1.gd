a87e54d13fbdbe6e6f5aa8321e5cc2f4e1e5156d
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2022-05-25 00:49:06 -0300

    Little adjustments.

--- scenes/gameplay/level_1/level_1.gd
+++ scenes/gameplay/level_1/level_1.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 func _ready():
-	if !OS.is_debug_build():
+	if OS.has_feature("standalone"):
 		print('Opening level_1 in debug mode.')
 		$Player.transform.origin = Vector3.ZERO
 
