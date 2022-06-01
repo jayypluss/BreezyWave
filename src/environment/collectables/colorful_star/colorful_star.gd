8c4a54708d2058e0165c7d5241642727dd0a1921
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2022-05-31 23:53:55 -0300

    Enhancing and Refactoring Level 1.

--- src/environment/collectables/colorful_star/colorful_star.gd
+++ src/environment/collectables/colorful_star/colorful_star.gd
@@ -15,7 +15,7 @@ func _ready():
 	if GameState.colorful_stars_collected.has(key):
 		queue_free()
 
-func _on_body_entered(body : KinematicBody):
+func _on_body_entered(body: KinematicBody):
 	player = body
 	t = 0.0
 	collected = true
@@ -24,7 +24,8 @@ func _on_body_entered(body : KinematicBody):
 func _physics_process(delta):
 	if collected:
 		t += delta * 2
-		mesh_instance.global_transform.origin = mesh_instance.global_transform.origin.linear_interpolate(player.global_transform.origin, t)
+		if is_instance_valid(mesh_instance) && is_instance_valid(player):
+			mesh_instance.global_transform.origin = mesh_instance.global_transform.origin.linear_interpolate(player.global_transform.origin, t)
 		quad_mesh.size = quad_mesh.size.linear_interpolate(Vector2.ZERO, t)
 
 		if quad_mesh.size == Vector2.ZERO:
