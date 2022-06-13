149005457ef32a95f25a1a91d15a26612c4943d0
jayypluss <jonathanfk1@gmail.com>
2022-06-13 18:39:38 -0300

    Adding input for showing collectables HUD.

--- src/gui/collected_stars_info/collected_stars_info.gd
+++ src/gui/collected_stars_info/collected_stars_info.gd
@@ -12,15 +12,15 @@ var is_star_counter_showing : bool = false
 func _ready():
 	update_label()
 	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("show_hud"):
+		show_hud()
+	
 func on_star_collected():
+	show_hud()
 	update_label()
 
 func update_label():
-	if !is_star_counter_showing:
-		animation_player.play("show")
-		is_star_counter_showing = true
-		
-	timer.start()
 	stars_collected = GameState.colorful_stars_collected.size()
 	counter_label.text = str(stars_collected)
 
@@ -28,3 +28,10 @@ func _on_Timer_timeout():
 	if is_star_counter_showing:
 		animation_player.play("hide")
 		is_star_counter_showing = false
+
+func show_hud():
+	if !is_star_counter_showing:
+		animation_player.play("show")
+		is_star_counter_showing = true
+		
+	timer.start()
