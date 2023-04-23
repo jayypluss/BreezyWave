54a0be5f112f47944f6f598d8999c3eb4e4300f3
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-04-23 19:01:22 -0300

    - fixes moving platform

--- src/environment/moving_platform/moving_platform.gd
+++ src/environment/moving_platform/moving_platform.gd
@@ -1,11 +1,23 @@
-extends Node3D
+extends AnimatableBody3D
+class_name MovingPlatform
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var duration: float = 7
+@export var move_z: float = -1
 
+var initial_z: float
+var final_z: float
+var tween: Tween
+
+func _ready() -> void:
+	initial_z = position.z
+	final_z = position.z + move_z
+  
 
 func start_moving():
-	animation_player.play("move")
-	animation_player.playback_active = true
+	tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_property(self, "position:z", final_z, duration)
+	tween.tween_property(self, "position:z", initial_z, duration)
 
 func stop_moving():
-	animation_player.playback_active = false
+	tween.stop()
