3e6db80ecf381dff6d49db3fcb2dd232fea97c3a
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-04-28 15:39:15 -0300

    - tests on tween animation for pedestal

--- src/environment/pedestal/pedestal.gd
+++ src/environment/pedestal/pedestal.gd
@@ -6,7 +6,7 @@ var go_up_tween: Tween
 var go_down_tween: Tween
 var label_initial_y: float
 var label_move_length_y: float = 1
-var label_tween_duration_y: float = 1
+var label_tween_duration_y: float = 0.5
 
 func _ready():
 	label_initial_y = label.position.y
@@ -19,26 +19,42 @@ func _ready():
 
 
 func _on_body_entered(body):
-	label.show()
+#	label.show()
 	start_moving()
 
 
 func _on_body_exited(body):
-	label.hide()
+#	label.hide()
 	stop_moving()
 	
 	
 func start_moving():
-	if !go_up_tween:
-		go_up_tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-		go_up_tween.tween_property(label, "position:y", (label_initial_y + label_move_length_y), label_tween_duration_y)
-	else:
-		go_up_tween.play()
+	if go_down_tween:
+		go_down_tween.stop()
+		go_down_tween.kill()
+#		go_down_tween = null
+	if go_up_tween:
+		go_up_tween.stop()
+		go_up_tween.kill()
+#		go_up_tween = null
+	go_up_tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT).set_parallel(true)
+	go_up_tween.tween_property(label, "position:y", (label_initial_y + label_move_length_y), label_tween_duration_y)
+	go_up_tween.tween_property(label, "font_size", 1500, label_tween_duration_y)
+	go_up_tween.tween_property(label, "outline_size", 24, label_tween_duration_y)
 
 func stop_moving():
-	go_up_tween.stop()
+	if go_up_tween:
+		go_up_tween.stop()
+		go_up_tween.kill()
+#		go_up_tween = null
 	var current_label_y = label.position.y
-	go_down_tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	if go_down_tween:
+		go_down_tween.stop()
+		go_down_tween.kill()
+#		go_down_tween = null
+	go_down_tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT).set_parallel(true)
 	go_down_tween.tween_property(label, "position:y", label_initial_y, label_tween_duration_y)
+	go_down_tween.tween_property(label, "font_size", 1, label_tween_duration_y)
+	go_down_tween.tween_property(label, "outline_size", 1, label_tween_duration_y)
 
 
