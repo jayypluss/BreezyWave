06b9f0fa8c4b1411c17b076af702e8a413a8afd6
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-04-28 11:55:41 -0300

    - changes on pedestal

---
+++ src/environment/pedestal/pedestal.gd
@@ -0,0 +1,44 @@
+extends Area3D
+
+@export var control: Node
+@onready var label: Label3D = $StaticBody3D/Label3D
+var go_up_tween: Tween
+var go_down_tween: Tween
+var label_initial_y: float
+var label_move_length_y: float = 1
+var label_tween_duration_y: float = 1
+
+func _ready():
+	label_initial_y = label.position.y
+	if is_instance_valid(control):
+		if control.has_method('_on_Player_entered_area'):
+			var _ben = self.connect("body_entered", Callable(control,"_on_Player_entered_area").bind(self))
+		if control.has_method('_on_Player_exited_area'):
+			var _bex = self.connect("body_exited", Callable(control,"_on_Player_exited_area").bind(self))
+
+
+
+func _on_body_entered(body):
+	label.show()
+	start_moving()
+
+
+func _on_body_exited(body):
+	label.hide()
+	stop_moving()
+	
+	
+func start_moving():
+	if !go_up_tween:
+		go_up_tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		go_up_tween.tween_property(label, "position:y", (label_initial_y + label_move_length_y), label_tween_duration_y)
+	else:
+		go_up_tween.play()
+
+func stop_moving():
+	go_up_tween.stop()
+	var current_label_y = label.position.y
+	go_down_tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	go_down_tween.tween_property(label, "position:y", label_initial_y, label_tween_duration_y)
+
+
--- src/game/tutorial/tutorial_control.gd
+++ src/game/tutorial/tutorial_control.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 @onready var step: AcceptDialog = $Step
+var area_player_is_inside: Area3D
 
 var tutorial_texts = [
 	"W, A, S, D: Walking \nSPACE: Jumping \nSHIFT: Sprinting", 
@@ -11,24 +12,29 @@ var tutorial_texts = [
 ]
 
 func _input(event):
-	if event.is_action_pressed("interact") && GameState.is_showing_tutorial_step:
+	if event.is_action_pressed("interact") && !GameState.is_showing_tutorial_step:
+		if area_player_is_inside:
+			show_step(area_player_is_inside)
+	elif event.is_action_pressed("interact") && GameState.is_showing_tutorial_step:
 		hide_popup()
 		
-func show_step(text_index: int):
-	step.dialog_text = tutorial_texts[text_index]
-	step.popup()
-	GameState.is_showing_tutorial_step = true
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	get_tree().paused = true
-	step.popup_centered()
+func show_step(area: Area3D):
+	if area.name.substr(4, 5):
+		var text_index: int = int(area.name.substr(4, 5))
+		if tutorial_texts.size() > text_index:
+			step.dialog_text = tutorial_texts[text_index]
+			step.popup()
+			GameState.is_showing_tutorial_step = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_tree().paused = true
+			step.popup_centered()
+			GameState.tutorial_steps.append(text_index)
 
 func _on_Player_entered_area(_player: Player, area: Area3D):
-	var text_index: int = int(area.name.substr(4, 5))
-	show_step(text_index)
-	GameState.tutorial_steps.append(text_index)
+	area_player_is_inside = area
 
 func _on_Player_exited_area(_player: Player, _area: Area3D):
-	hide_popup()
+	area_player_is_inside = null
 	
 func hide_popup():
 	step.hide()
