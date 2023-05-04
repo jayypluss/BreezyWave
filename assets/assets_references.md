ef2f9139b399003850dbca887f34e6bad61109f9
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-05-04 01:52:51 -0300

    - adding 2d galaxy shader to portal

--- assets/assets_references.md
+++ assets/assets_references.md
@@ -9,3 +9,8 @@ https://sketchfab.com/3d-models/skeleton-wings-e6ce6969975a46a392f533c6be472210
 #### Motion blur shader
 
 https://github.com/Bauxitedev/godot-motion-blur
+
+
+#### 2D "galaxy" Shader
+
+https://godotshaders.com/shader/galaxy-shader/
--- src/environment/portal/portal.gd
+++ src/environment/portal/portal.gd
@@ -5,6 +5,7 @@ signal on_finish_animation()
 
 @onready var enter_anim_position: Node3D = $EnterAnimPosition
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var portal_2d_shader_control = $Portal2DShaderControl
 
 var already_entered := false
 
@@ -18,11 +19,17 @@ func suck_node_into_portal(_body: Node3D):
 		_body.lock_movement()
 		var final_pos = mesh_instance_3d.global_position
 		final_pos.z -= 2
+		portal_2d_shader_control.show()
 		var tween: Tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT).set_parallel(true)
 		tween.tween_property(_body, "position", enter_anim_position.global_position, 0.5)
 		tween.tween_property(_body.player_pivot, "scale", Vector3(0.643, 0.197, 3.216), 0.7)
 		tween.chain().tween_property(_body, "position", final_pos, 0.2)
-		tween.chain().tween_callback(func(): emit_signal("on_finish_animation"); _body.lock_movement())
+		tween.chain().tween_callback(
+			func(): 
+				emit_signal("on_finish_animation")
+				_body.lock_movement()
+				portal_2d_shader_control.hide()
+		)
 		
 	
 	
