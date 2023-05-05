96505d82942ec442fb3d662e0beb90f8debf2604
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-05-05 20:20:53 -0300

    - changes on the ui

--- src/gui/credits_screen/credits_screen.gd
+++ src/gui/credits_screen/credits_screen.gd
@@ -1,6 +1,7 @@
 extends Control
 
-func _on_main_menu_button_toggled(_button_pressed):
-	print(get_tree().root)
+func _on_main_menu_button_pressed():
 	hide()
-	get_tree().root.hide()
+	if get_parent() is MenuPages:
+		get_parent().hide()
+
