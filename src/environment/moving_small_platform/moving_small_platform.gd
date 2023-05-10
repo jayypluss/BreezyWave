c6e211c89a707a85cfd0facdd5621c14fe011581
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-05-09 22:08:13 -0300

    - changes on level 2

--- src/environment/moving_small_platform/moving_small_platform.gd
+++ src/environment/moving_small_platform/moving_small_platform.gd
@@ -1,5 +1,5 @@
 extends AnimatableBody3D
-class_name MovingPlatform
+class_name MovingSmallPlatform
 
 @export var duration: float = 4
 @export var movement_vector: Vector3 = Vector3.ZERO
@@ -10,15 +10,15 @@ var tween: Tween
 var going_back: bool = false
 
 func _ready() -> void:
-	initial_position = global_position
+	initial_position = position
 	final_position = initial_position + movement_vector
 
 func start_moving():
 	tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	if not going_back:
-		tween.tween_property(self, "global_position", final_position, duration)
+		tween.tween_property(self, "position", final_position, duration)
 		tween.chain().tween_callback(func(): print('going_back = true'); going_back = true)
-	tween.tween_property(self, "global_position", final_position, duration)
+	tween.tween_property(self, "position", initial_position, duration)
 	tween.chain().tween_callback(func(): print('going_back = false'); going_back = false)
 
 func stop_moving():
