b957818759123b0def4c7b5228f70aa966c564fb
Jonathan F. Kuntz <jonathanfk1@gmail.com>
2023-07-14 21:24:05 -0300

    - better jumping mechanics - animation on jumping

--- src/player/input.gd
+++ src/player/input.gd
@@ -5,6 +5,7 @@ signal toggle_camera(options)
 
 @onready var player: Player
 @onready var dash_effect: GPUParticles3D = $"../PlayerPivot/Effects/DashEffect"
+@onready var jump_effect: GPUParticles3D = $"../PlayerPivot/Effects/JumpEffect"
 @onready var dash_timer = $"../PlayerPivot/Effects/DashTimer"
 
 @export var walk_speed := 14.0
@@ -35,78 +36,78 @@ var lock_movement := false
 var jump_multiplier := 1.0
 
 func force_jump(multiplier: float = 1.0):
-	jump_multiplier = multiplier
+  jump_multiplier = multiplier
 
 func _ready():
-	if %FirstPersonCamera.current:
-		first_person = true
-	player = self.owner
-	available_jumps = num_of_jumps
-	last_direction = Vector3.FORWARD.rotated(Vector3.UP,
-		%CameraPivot/Horizontal.global_transform.basis.get_euler().y).normalized()
+  if %FirstPersonCamera.current:
+    first_person = true
+  player = self.owner
+  available_jumps = num_of_jumps
+  last_direction = Vector3.FORWARD.rotated(Vector3.UP,
+    %CameraPivot/Horizontal.global_transform.basis.get_euler().y).normalized()
 
 func _on_toggle_camera(options):
-	var anim_duration:= 0.1
-	if options and options.anim_duration:
-		anim_duration = options.anim_duration
-	var tween: Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	if %Camera3D.current:
-		# Will become FIRST person
-		tween.tween_property(%Camera3D, "position", %FirstPersonCamera.position, anim_duration)
-		tween.tween_callback(
-			func(): 
-				%FirstPersonCamera.set_initial_position()			
-				first_person = true
-				%FirstPersonCamera.make_current()
-				GameState.player.set_meshes_visibility(false)
-				print(options)
-				if options and options.after_anim_action:
-					options.after_anim_action.call()
-		)
-	else:
-		# Will become THIRD person
-		tween.tween_property(%FirstPersonCamera, "position", %Camera3D.position, anim_duration)
-		tween.tween_callback(
-			func(): 
-				%CameraPivot.set_initial_position()
-				first_person = false
-				%Camera3D.make_current()
-				GameState.player.set_meshes_visibility(true)
-				print(options)
-				if options and options.after_anim_action:
-					options.after_anim_action.call()
-		)
+  var anim_duration:= 0.1
+  if options and options.anim_duration:
+    anim_duration = options.anim_duration
+  var tween: Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+  if %Camera3D.current:
+    # Will become FIRST person
+    tween.tween_property(%Camera3D, "position", %FirstPersonCamera.position, anim_duration)
+    tween.tween_callback(
+      func(): 
+        %FirstPersonCamera.set_initial_position()			
+        first_person = true
+        %FirstPersonCamera.make_current()
+        GameState.player.set_meshes_visibility(false)
+        print(options)
+        if options and options.after_anim_action:
+          options.after_anim_action.call()
+    )
+  else:
+    # Will become THIRD person
+    tween.tween_property(%FirstPersonCamera, "position", %Camera3D.position, anim_duration)
+    tween.tween_callback(
+      func(): 
+        %CameraPivot.set_initial_position()
+        first_person = false
+        %Camera3D.make_current()
+        GameState.player.set_meshes_visibility(true)
+        print(options)
+        if options and options.after_anim_action:
+          options.after_anim_action.call()
+    )
 
 func _physics_process(delta: float) -> void:
-	if paused: # Used to prevent movement during a cutscene.
-		return
-	
-	# Get direction vector based checked input.
-	var direction = Vector3(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		0,
-		Input.get_action_strength("move_back") - Input.get_action_strength("move_front")
-	)
-	
-	if lock_movement:
-		direction = Vector3.ZERO
-	
+  if paused: # Used to prevent movement during a cutscene.
+    return
+  
+  # Get direction vector based checked input.
+  var direction = Vector3(
+    Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+    0,
+    Input.get_action_strength("move_back") - Input.get_action_strength("move_front")
+  )
+  
+  if lock_movement:
+    direction = Vector3.ZERO
+  
 #	# Rotate direction based checked camera.
-	var horizontal_rotation
-	
-	if first_person:
-		horizontal_rotation = %FirstPersonCamera.global_transform.basis.get_euler().y
-		direction = direction.rotated(Vector3.UP, horizontal_rotation).normalized()
-	else:
-		horizontal_rotation = %CameraPivot/Horizontal.global_transform.basis.get_euler().y
-		direction = direction.rotated(Vector3.UP, horizontal_rotation).normalized()
-	
-	if direction != Vector3.ZERO: # Player is moving.
-		direction = direction.normalized()
-		last_direction = direction
-		
-		if !first_person:
-			%PlayerPivot.look_at(player.position + direction, Vector3.UP)
+  var horizontal_rotation
+  
+  if first_person:
+    horizontal_rotation = %FirstPersonCamera.global_transform.basis.get_euler().y
+    direction = direction.rotated(Vector3.UP, horizontal_rotation).normalized()
+  else:
+    horizontal_rotation = %CameraPivot/Horizontal.global_transform.basis.get_euler().y
+    direction = direction.rotated(Vector3.UP, horizontal_rotation).normalized()
+  
+  if direction != Vector3.ZERO: # Player is moving.
+    direction = direction.normalized()
+    last_direction = direction
+    
+    if !first_person:
+      %PlayerPivot.look_at(player.position + direction, Vector3.UP)
 #		else:
 #			var camera_direction = %FirstPersonCamera.project_ray_normal(get_viewport().get_visible_rect().size / 2)
 #			print(camera_direction)
@@ -117,68 +118,70 @@ func _physics_process(delta: float) -> void:
 #				Input.get_action_strength("move_back") - Input.get_action_strength("move_front")
 #			)
 #			%PlayerPivot.look_at(camera_direction, Vector3.UP)
-		
-		if Input.is_action_pressed("sprint"): # Running.
-			speed = run_speed
-		else: # Walking.
-			speed = walk_speed
-		
-	
-	if Input.is_action_just_pressed('toggle_camera'):
-		_on_toggle_camera(null)
-		
-	if has_dash:	
-		if Input.is_action_just_pressed('dash'):
-			if dash_available:
-				dash_available = !dash_available
-				if direction.x == 0 and direction.z == 0:
-					direction = last_direction
-				speed = dash_impulse
-				if dash_effect:
-					dash_effect.set_emitting(true)
-				dash_timer.start()
-	
-	if Input.is_action_pressed("jump") or jump_multiplier > 1.0:
-		if num_of_jumps == 1 and !player.is_on_floor() and player.get_slide_collision_count() == 0:
-			available_jumps = 0
-			jump_multiplier = 1.0
-			
-		if available_jumps > 0:
-			available_jumps -= 1
-			player.velocity.y = jump_impulse*jump_multiplier
-			last_direction = direction
-	
-	if has_glide:
-		if (Input.is_action_pressed("jump") && player.velocity.y < 0): # Glid
-			print('glide')
-			is_gliding = true
-			player.velocity.y -= fall_acceleration * glide_velocity_multiplier * delta
-			player.velocity.x = direction.x * speed
-			player.velocity.z = direction.z * speed
-		else:
-			is_gliding = false
-			player.velocity.y -= fall_acceleration * delta
-			player.velocity.x = direction.x * speed
-			player.velocity.z = direction.z * speed
-	else:
-		player.velocity.y -= fall_acceleration * delta
-		player.velocity.x = direction.x * speed
-		player.velocity.z = direction.z * speed
-		
-	# Assign move_and_slide to velocity prevents the velocity from accumulating.
-	player.set_velocity(player.velocity)
-	player.set_up_direction(Vector3.UP)
-	player.move_and_slide()
-	player.velocity = player.velocity
+    
+    if Input.is_action_pressed("sprint"): # Running.
+      speed = run_speed
+    else: # Walking.
+      speed = walk_speed
+    
+  
+  if Input.is_action_just_pressed('toggle_camera'):
+    _on_toggle_camera(null)
+    
+  if has_dash:	
+    if Input.is_action_just_pressed('dash'):
+      if dash_available:
+        dash_available = !dash_available
+        if direction.x == 0 and direction.z == 0:
+          direction = last_direction
+        speed = dash_impulse
+        if dash_effect:
+          dash_effect.set_emitting(true)
+        dash_timer.start()
+  
+  if Input.is_action_pressed("jump") or jump_multiplier > 1.0:
+    if num_of_jumps == 1 and !player.is_on_floor() and player.get_slide_collision_count() == 0:
+      available_jumps = 0
+      jump_multiplier = 1.0
+      
+    if available_jumps > 0:
+      available_jumps -= 1
+      player.velocity.y = jump_impulse*jump_multiplier
+      if jump_effect:
+        jump_effect.set_emitting(true)
+      last_direction = direction
+  
+  if has_glide:
+    if (Input.is_action_pressed("jump") && player.velocity.y < 0): # Glid
+      print('glide')
+      is_gliding = true
+      player.velocity.y -= fall_acceleration * glide_velocity_multiplier * delta
+      player.velocity.x = direction.x * speed
+      player.velocity.z = direction.z * speed
+    else:
+      is_gliding = false
+      player.velocity.y -= fall_acceleration * delta
+      player.velocity.x = direction.x * speed
+      player.velocity.z = direction.z * speed
+  else:
+    player.velocity.y -= fall_acceleration * delta
+    player.velocity.x = direction.x * speed
+    player.velocity.z = direction.z * speed
+    
+  # Assign move_and_slide to velocity prevents the velocity from accumulating.
+  player.set_velocity(player.velocity)
+  player.set_up_direction(Vector3.UP)
+  player.move_and_slide()
+  player.velocity = player.velocity
 
-	if player.is_on_floor() and player.get_slide_collision_count() > 0: # Reset jumps.
-		available_jumps = num_of_jumps
+  if player.is_on_floor() and player.get_slide_collision_count() > 0: # Reset jumps.
+    available_jumps = num_of_jumps
 
 func _on_dash_timer_timeout():
-	dash_available = true
+  dash_available = true
 
 
 func _on_tp_camera_enter_player(_body):
-	if _body is Player:
-		player.hide_eyes()
-		
+  if _body is Player:
+    player.hide_eyes()
+    
--- src/player/player.gd
+++ src/player/player.gd
@@ -15,62 +15,62 @@ extends CharacterBody3D
 var last_floor_position: Vector3 = Vector3(0, 3, 0)
 
 func _ready():
-	GameState.player = self
-	last_position_timer.start()
-	GameState.player.set_meshes_visibility(%Camera3D.current)
-	check_everything_is_not_null()
+  GameState.player = self
+  last_position_timer.start()
+  GameState.player.set_meshes_visibility(%Camera3D.current)
+  check_everything_is_not_null()
 
 func check_everything_is_not_null():
-	if !camera:
-		print('camera is null')
-	if !last_position_timer:
-		print('last_position_timer is null')
+  if !camera:
+    print('camera is null')
+  if !last_position_timer:
+    print('last_position_timer is null')
 
 func _get_configuration_warnings() -> PackedStringArray:
-	if not camera:
-		return PackedStringArray(["Missing camera node"])
-	else:
-		return []
+  if not camera:
+    return PackedStringArray(["Missing camera node"])
+  else:
+    return []
 
 func die():
 #	position = Vector3(-0.515, 5.134, -267.35)
-	position = last_floor_position
+  position = last_floor_position
 
 func _on_LastPositionTimer_timeout():
-	if (is_on_floor() && get_last_slide_collision() != null 
-		&& get_last_slide_collision().get_collider(0) is CSGMesh3D):
-		last_floor_position = position
+  if (is_on_floor() && get_last_slide_collision() != null 
+    && get_last_slide_collision().get_collider(0) is CSGMesh3D):
+    last_floor_position = position
 
-	last_position_timer.start()	
+  last_position_timer.start()
 
 func lock_trough_input(lock: bool = true):
-	input.lock_movement = lock
+  input.lock_movement = lock
 
 func lock_movement(lock: bool = true, except_y: bool = false):
-	set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, lock)
-	if !except_y:
-		set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Y, lock)
-	set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, lock)
+  set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, lock)
+  if !except_y:
+    set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Y, lock)
+  set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, lock)
 
 func lock_rotation(lock: bool = true, except_y: bool = false):
-	set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_X, lock)
-	if !except_y:
-		set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Y, lock)
-	set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Z, lock)
-	
+  set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_X, lock)
+  if !except_y:
+    set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Y, lock)
+  set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Z, lock)
+  
 func set_meshes_visibility(_visible: bool):
-	player_mesh.visible = _visible
-	left_eyeball.visible = _visible
-	right_eyeball.visible = _visible
+  player_mesh.visible = _visible
+  left_eyeball.visible = _visible
+  right_eyeball.visible = _visible
 
 func _on_toggle_camera(options):
-	input._on_toggle_camera(options)
-	show_eyes()
-	
+  input._on_toggle_camera(options)
+  show_eyes()
+  
 func hide_eyes():
-	left_eyeball.visible = false
-	right_eyeball.visible = false
-	
+  left_eyeball.visible = false
+  right_eyeball.visible = false
+  
 func show_eyes():
-	left_eyeball.visible = true
-	right_eyeball.visible = true
+  left_eyeball.visible = true
+  right_eyeball.visible = true
